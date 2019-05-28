87c4d5afe059990255915b74ee5000fce29c6ec1
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-28 07:47:10 +0300

    Initial commit

---
+++ src/Containers/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { withFirebase } from '../Firebase';
+
+const Home = ({ firebase }: Object) => (
+  <button onClick={() => firebase.auth.signOut()} type="button">SIGN OUT</button>
+)
+
+export default withFirebase(Home)
