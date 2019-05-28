4139d6fffaccac9523c0b4599bf15bc60aa8a5a0
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-28 10:09:41 +0300

    fix code style

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const Home = ({ firebase }: Object) => (
-  <button onClick={() => firebase.auth.signOut()} type="button">SIGN OUT</button>
-)
+  <button onClick={() => firebase.auth.signOut()} type="button">
+    SIGN OUT
+  </button>
+);
 
-export default withFirebase(Home)
+export default withFirebase(Home);
