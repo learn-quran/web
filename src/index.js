89d2e5b0115d280e80e31e19983fd0ecc4f2b8b2
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-28 10:04:52 +0300

    cleaned up files in src/

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ScaleLoader } from 'react-spinners';
+import PropTypes from 'prop-types';
+
+import Firebase, { FirebaseContext } from './Firebase';
+import { NavBar, Navigation } from './Navigation';
 
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 
-import App from './App';
-import Firebase, { FirebaseContext } from './Firebase';
+class App extends React.Component {
+  static propTypes = {
+    firebase: PropTypes.object,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.props.firebase.auth.onAuthStateChanged(user => {
+      this.setState({ loading: false });
+      user ? this.setState({ user }) : this.setState({ user: null });
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const { user, loading } = this.state;
+    return loading ? (
+      <ScaleLoader
+        sizeUnit={'px'}
+        size={150}
+        color={'#123abc'}
+        loading={loading}
+      />
+    ) : (
+      <Router>
+        <Fragment>
+          <NavBar user={user} />
+
+          <Navigation user={user} />
+        </Fragment>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <App />
+    <FirebaseContext.Consumer>
+      {firebase => <App firebase={firebase} />}
+    </FirebaseContext.Consumer>
   </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
