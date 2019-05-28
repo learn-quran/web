bb1cb9a15b1840939ec79a31585a996a86de7c9d
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-28 21:45:18 +0300

    removed material styling and added custom error messages for firebase

--- src/Containers/Signup.js
+++ src/Containers/Signup.js
@@ -3,12 +3,12 @@ import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { Redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 import { withFirebase } from '../Firebase';
+
+import '../Assets/stylesheets/Signup.scss';
 
 const SignupSchema = Yup.object().shape({
   password: Yup.string()
@@ -18,31 +18,8 @@ const SignupSchema = Yup.object().shape({
     .required('Email is required')
     .email('Invalid email'),
 });
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    flex: 1,
-    height: '70vh',
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexDirection: 'column',
-    width: theme.spacing(50),
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-  },
-  button: {
-    marginTop: theme.spacing(5),
-    marginBottom: theme.spacing(5),
-    margin: theme.spacing(1),
-  },
-}));
 
 const Signup = ({ firebase }: Object) => {
-  const styles = useStyles();
   const [isSubmitting, changeIsSubmitting] = useState(false);
   return (
     <Formik
@@ -60,59 +37,69 @@ const Signup = ({ firebase }: Object) => {
             firebase.auth
               .createUserWithEmailAndPassword(values.email, values.password)
               .then(() => <Redirect to={{ pathname: '/' }} />)
-              .catch(function(error) {
-                toast.error(error.message);
+              .catch(function(err) {
+                let error = null;
+                switch (err.code) {
+                  case 'auth/email-already-in-use':
+                    error = 'This email address has already been taken';
+                    break;
+                  case 'auth/invalid-email':
+                    error = 'Invalid e-mail address format';
+                    break;
+                  case 'auth/weak-password':
+                    error = 'Password too weak';
+                    break;
+                  default:
+                    error = 'Check your internet connection';
+                }
+                toast.error(error || err.message);
                 changeIsSubmitting(false);
               });
           })
           .catch(err => {
-            toast.error(err.message);
+            toast.error(err);
             changeIsSubmitting(false);
           });
       }}
       render={({ values, handleBlur, handleChange, handleSubmit }) => (
-        <Grid
-          container
-          className={styles.root}
-          justify="center"
-          alignItems="center">
-          <form
-            className={styles.container}
-            onSubmit={handleSubmit}
-            autoCapitalize="off"
-            autoComplete="off">
-            <TextField
-              id="email"
-              label="Email "
-              type="email"
-              className={styles.textField}
-              value={values.email}
-              onChange={handleChange('email')}
-              onBlur={handleBlur('email')}
-              margin="normal"
-              variant="outlined"
-            />
-            <TextField
-              id="password"
-              type="password"
-              label="Password "
-              className={styles.textField}
-              value={values.password}
-              onChange={handleChange('password')}
-              onBlur={handleBlur('password')}
-              margin="normal"
-              variant="outlined"
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              className={styles.button}
-              onClick={handleSubmit}
-              disabled={isSubmitting}>
-              Sign up
-            </Button>
-          </form>
-        </Grid>
+        <form onSubmit={handleSubmit} autoCapitalize="off" autoComplete="off">
+          <div className="form-container">
+            <div className="text-field-container">
+              <TextField
+                id="email"
+                label="Email "
+                type="email"
+                className="text-field"
+                value={values.email}
+                onChange={handleChange('email')}
+                onBlur={handleBlur('email')}
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                id="password"
+                type="password"
+                label="Password "
+                className="text-field"
+                value={values.password}
+                onChange={handleChange('password')}
+                onBlur={handleBlur('password')}
+                margin="normal"
+                variant="outlined"
+              />
+            </div>
+            <div className="button-container">
+              <Button
+                variant="contained"
+                color="primary"
+                className="button"
+                onClick={handleSubmit}
+                disabled={isSubmitting}>
+                Sign up
+              </Button>
+            </div>
+          </div>
+        </form>
       )}
     />
   );
