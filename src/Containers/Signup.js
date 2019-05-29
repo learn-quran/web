178030289cecec37dc135c5e72077c97e7b789ce
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-29 04:40:23 +0300

    moved createUser to Firebase class and persist unique username

--- src/Containers/Signup.js
+++ src/Containers/Signup.js
@@ -12,58 +12,62 @@ import { withFirebase } from '../Firebase';
 import '../Assets/stylesheets/Signup.scss';
 
 const SignupSchema = Yup.object().shape({
-  password: Yup.string()
-    .required('Password is required')
-    .min(6, 'Password too short'),
   email: Yup.string()
     .required('Email is required')
     .email('Invalid email'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password too short'),
+  passwordConfirmation: Yup.string().required(
+    'Password confirmation is required',
+  ),
 });
 
 const Signup = ({ firebase }) => {
   const [isSubmitting, changeIsSubmitting] = useState(false);
+  const submit = values => {
+    if (values.password !== values.passwordConfirmation) {
+      toast.error("Passwords don't match");
+    } else {
+      changeIsSubmitting(true);
+      SignupSchema.validate(values, {
+        strict: true,
+        stripUnknown: true,
+      })
+        .then(() => {
+          firebase
+            .createUser(values)
+            .then(() => <Redirect to={{ pathname: '/' }} />)
+            .catch(error => {
+              toast.error(error);
+              changeIsSubmitting(false);
+            });
+        })
+        .catch(({ message }) => {
+          if (message !== 'NO_MESSAGE') {
+            toast.error(message);
+          }
+          changeIsSubmitting(false);
+        });
+    }
+  };
+  const handleKeyPress = ({ keyCode, charCode }, values) => {
+    if (keyCode === 13 || charCode === 13) {
+      submit(values);
+    }
+  };
+
   return (
     <Formik
       initialValues={{
         email: '',
+        username: '',
         password: '',
+        passwordConfirmation: '',
       }}
-      onSubmit={values => {
-        changeIsSubmitting(true);
-        SignupSchema.validate(values, {
-          strict: true,
-          stripUnknown: true,
-        })
-          .then(() => {
-            firebase.auth
-              .createUserWithEmailAndPassword(values.email, values.password)
-              .then(() => <Redirect to={{ pathname: '/' }} />)
-              .catch(function(err) {
-                let error = null;
-                switch (err.code) {
-                  case 'auth/email-already-in-use':
-                    error = 'This email address has already been taken';
-                    break;
-                  case 'auth/invalid-email':
-                    error = 'Invalid e-mail address format';
-                    break;
-                  case 'auth/weak-password':
-                    error = 'Password too weak';
-                    break;
-                  default:
-                    error = 'Check your internet connection';
-                }
-                toast.error(error || err.message);
-                changeIsSubmitting(false);
-              });
-          })
-          .catch(err => {
-            toast.error(err);
-            changeIsSubmitting(false);
-          });
-      }}
+      onSubmit={submit}
       render={({ values, handleBlur, handleChange, handleSubmit }) => (
-        <form onSubmit={handleSubmit} autoCapitalize="off" autoComplete="off">
+        <form autoCapitalize="off" autoComplete="off">
           <div className="form-container">
             <div className="text-field-container">
               <TextField
@@ -78,6 +82,17 @@ const Signup = ({ firebase }) => {
                 variant="outlined"
               />
               <TextField
+                id="username"
+                label="Username "
+                className="text-field"
+                value={values.username}
+                helperText="This is will be used on the leaderboard"
+                onChange={handleChange('username')}
+                onBlur={handleBlur('username')}
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
                 id="password"
                 type="password"
                 label="Password "
@@ -87,6 +102,18 @@ const Signup = ({ firebase }) => {
                 onBlur={handleBlur('password')}
                 margin="normal"
                 variant="outlined"
+              />
+              <TextField
+                id="passwordConfirmation"
+                type="password"
+                label="Password Confirmation "
+                className="text-field"
+                margin="normal"
+                variant="outlined"
+                value={values.passwordConfirmation}
+                onChange={handleChange('passwordConfirmation')}
+                onBlur={handleBlur('passwordConfirmation')}
+                onKeyDown={e => handleKeyPress(e, values)}
               />
             </div>
             <div className="button-container">
