f2d4ce4c6fd9a1cd974c606c63e9e464af788fdb
mohshbool <mohammad.alshbool2002@gmail.com>
2019-07-23 14:10:12 +0300

    add getRadnomInt()

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,4 +1,4 @@
-export const objectToArray = object => {
+const objectToArray = object => {
   let array = [];
   for (let values of Object.values(object)) {
     if (values.id || values.uid) array.push(values);
@@ -6,3 +6,8 @@ export const objectToArray = object => {
   }
   return array;
 };
+
+const getRandomInt = (max, min = 0) =>
+  Math.floor(Math.random() * (max - min - 1) + min);
+
+export { objectToArray, getRandomInt };
