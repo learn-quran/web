785a6e3ec5313155e8dd0f59ae1f558254383d5d
mohshbool <mohammad.alshbool2002@gmail.com>
2019-07-23 14:10:36 +0300

    add shuffleArray()

--- src/Helpers.js
+++ src/Helpers.js
@@ -10,4 +10,11 @@ const objectToArray = object => {
 const getRandomInt = (max, min = 0) =>
   Math.floor(Math.random() * (max - min - 1) + min);
 
-export { objectToArray, getRandomInt };
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+export { objectToArray, getRandomInt, shuffleArray };
