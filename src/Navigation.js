daa50c1f28089bbe694843d697f87641303e9592
mohshbool <alshboolmoh@gmail.com>
2019-08-18 03:51:51 +0300

    add privacy policy page

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,11 +6,13 @@ import Leaderboard from './Containers/Leaderboard';
 import Player from './Containers/Player';
 
 import Landing from './Containers/Landing';
+import PrivacyPolicy from './Containers/PrivacyPolicy';
 
 const Navigation = ({ user }) => (
   <Switch>
     <Route exact path="/" component={user ? Player : Landing} />
     <Route exact path="/leaderboard" component={Leaderboard} />
+    <Route exact path="/privacy-policy" component={PrivacyPolicy} />
     <Route render={() => <Redirect to="/" />} />
   </Switch>
 );
